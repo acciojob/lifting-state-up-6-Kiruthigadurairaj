951f3251c5d3d6bcada23891b564f6442fc2fe18
kiruthiga durairaj <kiruthigalatha05@gmail.com>
2025-03-16 16:28:18 +0530

    my first message

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,49 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [todos, setTodos] = useState([
+    { title: "Learn React", completed: false },
+    { title: "Build a React App", completed: false },
+    { title: "Deploy the React App", completed: false },
+  ]);
 
-export default App
+  const handleCompleteTodo = (index) => {
+    const updatedTodos = todos.map((todo, i) => {
+      if (i === index) {
+        return { ...todo, completed: true };
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div id="main">
+      <h1>Parent Component</h1>
+      <ChildComponent todos={todos} handleCompleteTodo={handleCompleteTodo} />
+    </div>
+  );
+};
+
+const ChildComponent = ({ todos, handleCompleteTodo }) => {
+  return (
+    <div id="child">
+      {/* Do not remove the main div */}
+      <h2>Child Component</h2>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo.title}
+            {!todo.completed && (
+              <button onClick={() => handleCompleteTodo(index)}>Complete</button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
